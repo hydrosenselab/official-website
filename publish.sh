cda50d2f16f40dacdf6678d21e5e42cff7b0c221
Manabendra Saharia <msaharia@iitd.ac.in>
2022-01-26 19:19:34 +0530

    Changed publish script

--- publish.sh
+++ publish.sh
@@ -6,9 +6,4 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating master branch"
-cd public && git add --all && git commit -m "Publishing to Github Pages (publish.sh)"
-
-git push
-
 scp -r public/* msaharia@hydrosense.iitd.ac.in:/var/www/hydrosense/https/html
